3d71871461adb8c8590aee3870f8ca0007ba8135
vanyalaptii <vanya.lapty@gmail.com>
2023-04-13 23:02:20 +0300

    Was remake extension with using algorithm instead a ".reversed()" method

--- Task2.playground/Contents.swift
+++ Task2.playground/Contents.swift
@@ -6,7 +6,19 @@ let x = 121
 
 extension Int {
     func isPallindrom() -> Bool {
-        String(self) == String(String(self).reversed())
+//        String(self) == String(String(self).reversed())
+        var originalString: [Character] = []
+        var reversedString: [Character] = []
+        
+        for letter in String(self) {
+            originalString.append(letter)
+        }
+        
+        for index in stride(from: originalString.count-1, through: 0, by: -1) {
+            reversedString.append(originalString[index])
+        }
+        
+        return reversedString == originalString ?  true :  false
     }
 }
 
