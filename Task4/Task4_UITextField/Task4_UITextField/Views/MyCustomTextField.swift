2d76b00526f01693460f33feee3e5cc8b0a4bd8e
vanyalaptii <vanya.lapty@gmail.com>
2023-04-21 23:40:01 +0300

    Was added a new functionality: - Create a callback that will be executed when return button on the keyboard presses. - Create a callback that will be executed when the Custom Text Field will become focused. - Create a functionality to add any UIControl to the left of the Custom Text Field. - Add a functionality to set minimum number of characters required to enter. - Add a functionality to change all Custom Text Field components color to red if the number of characters is not equal to predefined min number of characters (error state). - When the text is typed the placeholder must become smaller and be moved to the top left corner of the Custom Text Field. - Add a line under the text that will become bolder (or change the color to darker one) when the Custom Text Field becomes focused.

--- Task4/Task4_UITextField/Task4_UITextField/Views/MyCustomTextField.swift
+++ Task4/Task4_UITextField/Task4_UITextField/Views/MyCustomTextField.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 // Define a custom text field class that inherits from UITextField
-class MyCustomTextField: UITextField {
-    
+class MyCustomTextField: UITextField, UITextFieldDelegate {
     // Define a callback closure that takes a string parameter
     var textChangedCallback: ((String) -> Void)?
     var editingDidEndCallback: ((String) -> Void)?
+    var editingDidBeginCallback: (() -> Void)?
     
-    // Define a label to display the character count
-    private let characterCountLabel: UILabel = {
-        let label = UILabel()
+    let underlineLayer = CALayer()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel(frame: CGRect(origin: .zero, size: CGSize(width: 40, height: 10)))
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .right
-        label.font = UIFont.systemFont(ofSize: 12)
-        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 12, weight: .thin)
+        label.textColor = .white
+        label.isHidden = false
         return label
     }()
     
@@ -32,29 +34,74 @@ class MyCustomTextField: UITextField {
         }
     }
     
+    // Define a label to display the character count
+    private let characterCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .right
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .white
+        label.isHidden = false
+        return label
+    }()
+    
+    
     // Initialize the custom text field
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupCharacterCountLabel()
+        setupSubviews()
         // Add a target for editingChanged event
         addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         addTarget(self, action: #selector(textFieldDidEndEditing), for: .editingDidEnd)
+        addTarget(self, action: #selector(textFieldDidBeginEditing), for: .editingDidBegin)
+        delegate = self
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupCharacterCountLabel()
+        setupSubviews()
         // Add a target for editingChanged event
         addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         addTarget(self, action: #selector(textFieldDidEndEditing), for: .editingDidEnd)
+        addTarget(self, action: #selector(textFieldDidBeginEditing), for: .editingDidBegin)
+        delegate = self
     }
     
-    private func setupCharacterCountLabel() {
-            // Add the character count label as a subview and configure constraints
-            addSubview(characterCountLabel)
-            characterCountLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5).isActive = true
-            characterCountLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5).isActive = true
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Adjust the placeholder label position and size
+        if !isEditing && text?.isEmpty ?? true {
+            let x = placeholderLabel.frame.origin.x
+            let y = placeholderLabel.frame.origin.y
+            let width = placeholderLabel.intrinsicContentSize.width
+            let height = placeholderLabel.intrinsicContentSize.height
+            placeholderLabel.frame = CGRect(x: x, y: y, width: width, height: height)
+        } else {
+            placeholderLabel.frame = CGRect(x: 30, y: 4, width: placeholderLabel.intrinsicContentSize.width, height: placeholderLabel.intrinsicContentSize.height)
         }
+    }
+    
+    private func setupSubviews() {
+        // Add the character count label as a subview and configure constraints
+        addSubview(placeholderLabel)
+        addSubview(characterCountLabel)
+        NSLayoutConstraint.activate([
+            characterCountLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            characterCountLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+        
+        // Set up the underline layer
+        underlineLayer.backgroundColor = UIColor.gray.cgColor
+        underlineLayer.frame = CGRect(x: 5, y: frame.height, width: frame.width - 10, height: 1)
+        layer.addSublayer(underlineLayer)
+                
+    }
+    
+    func addLeftControl(_ control: UIControl) {
+        leftView = control
+        leftViewMode = .whileEditing
+    }
     
     // Helper method to handle the editingChanged event
     @objc private func textFieldDidChange() {
@@ -62,11 +109,61 @@ class MyCustomTextField: UITextField {
         textChangedCallback?(text ?? "")
         // Update the character count label with the current text length
         characterCountLabel.text = "\(text?.count ?? 0)"
+       
+        if text != "" {
+            placeholderLabel.text = placeholder
+        } else {
+            placeholderLabel.text = ""
+        }
+        
+        if (text ?? "").count < 8 {
+            characterCountLabel.textColor = .red
+            characterCountLabel.text = "Min 8 symbols required"
+        } else {
+            characterCountLabel.textColor = .white
+        }
+        
+        updateUnderline()
     }
+    
     // Helper method to handle the editingDidEnd event
     @objc private func textFieldDidEndEditing() {
         // Call the callback closure with the text value
         editingDidEndCallback?(text ?? "")
+    }
+    
+    @objc private func textFieldDidBeginEditing() {
+        editingDidBeginCallback?()
+    }
+    
+    @objc func clearTextField() {
+        text = ""
+        characterCountLabel.text = "\(text?.count ?? 0)"
+        placeholderLabel.text = ""
+        characterCountLabel.textColor = .red
+    }
+    
+    @objc private func textDidChange(_ textField: UITextField) {
+        setNeedsLayout()
+    }
+    
+    private func updateUnderline() {
+        let color = isEditing ? UIColor.cyan.cgColor : UIColor.gray.cgColor
+        let width: CGFloat = isEditing ? 2 : 1
+        underlineLayer.backgroundColor = color
+        underlineLayer.frame = CGRect(x: 3, y: frame.height - width, width: frame.width - 6, height: width)
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        let result = super.becomeFirstResponder()
+        updateUnderline()
+        return result
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        let result = super.resignFirstResponder()
+        updateUnderline()
+        return result
     }
 }
 
